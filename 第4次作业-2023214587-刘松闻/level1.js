19864a71cf98dd2220b00844d84f1c3352cb2a2b
liamcq <liusonwen@outlook.com>
2024-12-24 02:12:54 +0800

    第四次提交

---
+++ 第4次作业-2023214587-刘松闻/level1.js
@@ -0,0 +1,21 @@
+let person = {
+    name: "Alan",
+    age: 20,
+};
+console.log(person);
+
+
+
+let person2 = new Object();
+person2.name = "Alex";
+person2.age = 30;
+console.log(person2);
+
+
+function Person3(name, age){
+    this.name = name;
+    this.age = age;
+}
+
+let person3 = new Person3("Bob", 25);
+console.log(person3);
